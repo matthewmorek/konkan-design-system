9de9a269fd0a9e36e4d6ed54970cec6094ef4e61
Matthew Morek <hi@matthewmorek.com>
2023-11-06 10:12:04 +0000

    ✨ [web] Add and configure new platform target

--- src/index.ts
+++ src/index.ts
@@ -69,12 +69,7 @@ StyleDictionary.registerTransform(hexToHex8Transformer);
 StyleDictionary.registerTransform(mathTransformer);
 
 registerTransforms(StyleDictionary, {
-  expand: {
-    composition: true,
-    typography: true,
-    border: false,
-    shadow: false,
-  },
+  expand: false,
   excludeParentKeys: false,
   "ts/color/modifiers": {
     format: "srgb",
@@ -82,7 +77,7 @@ registerTransforms(StyleDictionary, {
 });
 
 // declare transforms to be used
-const transforms: string[] = [
+const mobileTransforms: string[] = [
   "attribute/color",
   "attribute/cti",
   "ts/opacity",
@@ -91,6 +86,25 @@ const transforms: string[] = [
   "tceu/color/rgba/hex8",
   "tceu/color/hex/hex8",
   "tceu/resolveMath",
+];
+
+const webTransforms: string[] = [
+  "attribute/color",
+  "attribute/cti",
+  "ts/size/px",
+  "ts/opacity",
+  "ts/size/lineheight",
+  "ts/typography/fontWeight",
+  "ts/resolveMath",
+  "tceu/resolveMath",
+  "ts/size/css/letterspacing",
+  "ts/typography/css/fontFamily",
+  "ts/typography/css/shorthand",
+  "ts/border/css/shorthand",
+  "ts/shadow/css/shorthand",
+  "ts/color/css/hexrgba",
+  "ts/color/modifiers",
+  "name/cti/kebab",
 ];
 
 // allowed types of design tokens to be output
@@ -122,7 +136,7 @@ async function run() {
       platforms: {
         mobile: {
           buildPath: "./dist/",
-          transforms: transforms,
+          transforms: mobileTransforms,
           files: [
             {
               filter: (token: DesignToken) =>
@@ -132,6 +146,21 @@ async function run() {
                 token.attributes?.category === "semantic",
               destination: `${name.toLowerCase()}.json`,
               format: "json/nested",
+            },
+          ],
+        },
+        web: {
+          buildPath: "./dist/",
+          transforms: webTransforms,
+          files: [
+            {
+              filter: (token: DesignToken) =>
+                // temporarily filter out anything other than colours
+                allowedTypes.includes(token.type) &&
+                // we only want semantic tokens
+                token.attributes?.category === "semantic",
+              destination: `${name.toLowerCase()}.css`,
+              format: "css/variables",
             },
           ],
         },
